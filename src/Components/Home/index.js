15238ff782c3dac61eb6df22b597149b7cb41cba
chrisliew <chriswvliew@gmail.com>
2019-07-13 07:45:09 +0700

    added heroku

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,7 +4,7 @@ class Home extends Component {
   render() {
     return (
       <div className='home'>
-        <div>Sup it's home</div>
+        <div>Sup it's home, yeah my dude</div>
       </div>
     );
   }
