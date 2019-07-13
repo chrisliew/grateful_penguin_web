e97159b3588622ff910ab22851fe6db7d201b352
chrisliew <chriswvliew@gmail.com>
2019-07-13 07:08:14 +0700

    first commit

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <header>
+          <h3 className='header__title'>Grateful Penguin</h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
